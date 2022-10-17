c903d858795f8351ff2154061d0fbb22e7f6179d
Matt Simon Enriquez <matt.enriquez3@mail.dcu.ie>
2022-10-17 22:29:40 +0100

    Minor edit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hl2&g6xkgs4sygilgp_z3ivn+c%*=^g*9=k!n_)o5^dr8)_b&v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://shielded-bayou-41862.herokuapp.com/']
 
 
 # Application definition
